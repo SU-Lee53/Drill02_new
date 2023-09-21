e76329ff78d652d46feea5161826fde90aa8da94
SU-Lee53 <dltmddnr05@naver.com>
2023-09-21 10:42:11 +0900

    사각형 완성

--- character_circle_rectangle.py
+++ character_circle_rectangle.py
@@ -29,9 +29,14 @@ def run_ractangle():
     for x in range(50, 750+1, 10):
         render_frame(x,90)
 
-    for x in range(750, 50, -10):
-        render_frame(x,550)
+    for y in range(90, 550+1, 10):
+        render_frame(x,y)
     
+    for x in range(750, 50-1, -10):
+        render_frame(x,y)
+
+    for y in range(550, 90-1, -10):
+        render_frame(x,y)
 
 while True:
     # run_circle()
