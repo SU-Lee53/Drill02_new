9dd578918855447f10cc7c8acdd634fb9005826c
SU-Lee53 <dltmddnr05@naver.com>
2023-09-21 09:58:19 +0900

    기본적인 무한루프 뼈대 완성

--- character_circle_rectangle.py
+++ character_circle_rectangle.py
@@ -1,8 +1,24 @@
 from pico2d import *
+import math
+
 
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
+
+def run_circle():
+    print("circle")
+    pass
+
+def run_ractangle():
+    print("ractangle")
+    pass
+
+
+while True:
+    run_circle()
+    run_ractangle()
+
 
 close_canvas()
