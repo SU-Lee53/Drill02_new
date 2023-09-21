e05d4891a7a690525ef7b58ffb857656603ec8ad
SU-Lee53 <dltmddnr05@naver.com>
2023-09-21 10:35:21 +0900

    캐릭터 그리긱 함수화 완료

--- character_circle_rectangle.py
+++ character_circle_rectangle.py
@@ -7,6 +7,12 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def render_frame(x, y):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    delay(0.01)
+    
 def run_circle():
     print("circle")
 
@@ -15,26 +21,25 @@ def run_circle():
     for deg in range(0, 360, 1):
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        delay(0.01)
+        render_frame(x,y)
 
 def run_ractangle():
     print("ractangle")
 
     for x in range(50, 750+1, 10):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, 90)
-        delay(0.01)
+        render_frame(x,90)
 
+    for y in range(90, 550+1, 10):
+        render_frame(x, y)
         
+    for x in range(750, 50, -10):
+        render_frame(x,550)
+    
     pass
 
 
 while True:
-    # run_circle()
+    run_circle()
     run_ractangle()
     break
 
